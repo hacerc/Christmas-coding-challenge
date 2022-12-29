f22232868f682b19294d01a0abd8e657ff8ec8e5
hacerc <hcrcpkn@gmail.com>
2022-12-29 23:58:17 +0000

    fetch api

--- day-12/script.js
+++ day-12/script.js
@@ -1,20 +1,37 @@
-const boxes= document.querySelectorAll('.box')
+const boxes = document.querySelectorAll(".box");
+const pokeApi = "https://pokeapi.co/api/v2";
+window.addEventListener("scroll", checkBoxes);
 
-window.addEventListener('scroll', checkBoxes)
+checkBoxes();
 
-checkBoxes()
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-function checkBoxes () {
-    const triggerBottom = window.innerHeight/ 5 * 4
-
-    boxes.forEach (box => {
-        const boxTop = box.getBoundingClientRect().top
-
-        if (boxTop < triggerBottom) {
-            box.classList.add ('show')
-    
-        } else {
-            box.classList.remove ('show')
-        }
-    })
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    console.log(boxTop);
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
+
+const picture = async () => {
+  const selectedBox = document.querySelector(".selected-box");
+  const results = await getAllComments();
+  results.forEach((item) => {
+    selectedBox.innerHTML += `<div class="box"><h2>${item.email}</h2></div>`;
+    console.log(selectedBox.innerHTML);
+    console.log(item);
+
+  });
+};
+const getAllComments = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
+picture();
